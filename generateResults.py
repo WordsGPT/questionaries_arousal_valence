6f80aa34ae87b5c3d5dd9d1566a2013f1b5b8c97
Gus <gustavo.vy@gmail.com>
2026-01-31 15:15:21 +0100

    generateResults.py probably finished.

--- generateResults.py
+++ generateResults.py
@@ -1,198 +1,72 @@
+import sys
 import json
-import csv
-from pathlib import Path
-import numpy as np
+from   pathlib import Path
 import re
 import time
 import pandas as pd
-import sys
+import collections
 
 """ USAGE:
-python generateResults_json_output.py [EXPERIMENT_PATH][mode] [language]
 
-modes: 
-- json (output of estimations is a JSON with the word and its prediction.
-  It checks if the word in the input matches the word in the output)
-- weighted_sum (wheighted sum of the logprobs of the tokens in the word.
-  Only valid if single token output)
-- number (output of estimations is a number, the estimation of the word)
+python generateResults_json_output.py [EXPERIMENT_PATH]
 
-languages:
-- german
 """
-
-def extract_word_input(text):
-    match = re.search(f'{open_quotations_constant}(.*?){closing_quotations_constant}', text)
-    if match:
-        word = match.group(1)
-        return word
-    return None
-
-
-def extract_word_output(text):
-    match = re.search(f'"{word_constant}"' + r'\s*:\s*"([^"]+)"', text)
-    if match:
-        word = match.group(1)
-        return word
-    return None
-
-
-def extract_number(text):
-    match = re.search(f'"{feature_constant}' + r'"\s*:\s*"([0-9]*\.?[0-9]+)"', text)
-    if not match:
-        match = re.search(f'"{feature_constant}' + r'"\s*:\s*([0-9]*\.?[0-9]+)', text)
-    if not match:
-        all_matches = re.findall(r'[-+]?\d*\.\d+|[-+]?\d+\.\d*|[-+]?\d+', text)
-        if all_matches:
-            return float(all_matches[-1])
-    if match:
-        return float(match.group(1))
-    return None
-
 
 def read_jsonl(file_path):
     with open(file_path, 'r', encoding='utf-8') as f:
         return [json.loads(line) for line in f]
-
-## OpenAI ##
-
-def openAI_processing(results_content_file, batches_content_file):
-    match_key = 'custom_id'
-    lookup = {entry[match_key]: entry for entry in batches_content_file if match_key in entry}
-    combined_data = []
-    index = 0
-
-    for entry in results_content_file:
-        entry_result = {}
-        index += 1
-        weighted_sum = None
-        logprob = None
-        word_input = None
-        feature_value = None
-
-        if match_key in entry and entry[match_key] in lookup:
-           combined_entry = {**entry, **lookup[entry[match_key]]}
-           
-           if index == 1: print(combined_entry.keys())
-           
-           custom_id = combined_entry['custom_id']
-           if mode == "json":             
-              word_input = extract_word_input(combined_entry['body']['messages'][0]['content'])
-              word_output = extract_word_output(combined_entry['response']['body']['choices'][0]['message']['content'])
-
-              feature_value = extract_number(combined_entry['response']['body']['choices'][0]['message']['content'])
-              if word_input and word_output:
-                 if word_input != word_output:
-                    print(f"Warning: custom Id: '{custom_id}. Word input '{word_input}' does not match word output '{word_output}'")
-                    #feature_value = '#N/D'
-
-           elif mode == "weighted_sum" or mode == "number":
-              word_input = extract_word_input(combined_entry['body']['messages'][0]['content'])
-              # Only valid for responses of single token
-              if len(combined_entry["response"]["body"]["choices"][0]["logprobs"]["content"]) == 1:
-                 top_logprobs_list = combined_entry["response"]["body"]["choices"][0]["logprobs"]["content"][0]['top_logprobs']
-                 weighted_sum = 0
-                 total_prob = 0
-                 # Iterate over the list of top_logprobs that are numbers
-                 for top_logprob in top_logprobs_list:
-                     try:
-                         token_value = int(top_logprob['token'])
-                         logprob_value = top_logprob['logprob']
-                         weighted_sum += token_value * np.exp(float(logprob_value))
-                         total_prob += np.exp(float(logprob_value))
-                     except ValueError:
-                         pass
-                 weighted_sum /= total_prob if total_prob > 0 else 1
-                 logprob = combined_entry['response']['body']['choices'][0]['logprobs']['content'][0]['logprob']
-              feature_value = combined_entry['response']['body']['choices'][0]['message']['content']
-
-           entry_result['word'] = word_input
-           # entry_result['custom_id'] = custom_id
-           entry_result[feature_column] = feature_value
-
-           if logprob is not None:
-              entry_result['logprob'] = logprob
-              logprobs = True
-           if weighted_sum is not None:
-              entry_result['weighted_sum'] = weighted_sum
-
-           combined_data.append(entry_result)
-
-    return combined_data
 
 ## Main ##
 
 if __name__ == "__main__":
     if len(sys.argv) > 1:
        EXPERIMENT_PATH = sys.argv[1]
-
-    if len(sys.argv) > 2:
-       mode = sys.argv[2]
-       if len(sys.argv) > 3:
-          country = sys.argv[3]
-       else:
-          country = ""
     else:
         print(
-            "Provide as arguments the experiment path, the mode\
-             [json, number, weighted_sum] and optionally the language."
+            "Provide as arguments the experiment path (only)."
         )
         exit()
 
-    open_quotations_constant = '"'
-    closing_quotations_constant = '"'
-    # TO CONFIGURE DEPENDING ON YOUR CASE STUDY #
-    word_constant = 'Word'
-    feature_column = 'familiarity'
-    feature_constant = 'AoA'
-    # END TO CONFIGURE DEPENDING ON YOUR CASE STUDY #
-    logprobs = False
     timestamp = int(time.time())
     output_file = f'{EXPERIMENT_PATH}/output_{timestamp}.xlsx'
 
-    if country == 'german':
-        # TO CONFIGURE DEPENDING ON YOUR CASE STUDY #
-        word_constant = 'Wort'
-        feature_constant = 'Erwerbsalter'
-        # END TO CONFIGURE DEPENDING ON YOUR CASE STUDY #
-        open_quotations_constant = '„'
-        closing_quotations_constant = '”'
-
-    #results_file = f"{EXPERIMENT_PATH}/results/results.jsonl"
-    #batches_file = f"{EXPERIMENT_PATH}/batches/batches.jsonl"
-    results_file = f"{EXPERIMENT_PATH}/batch_69768be725ec81909eff329e70702946_output.jsonl"
-    batches_file = f"{EXPERIMENT_PATH}/questionaries_arousal_valence_v01_sample_batch_0_2026-01-25-22-29.jsonl"
+    results_file = f"{EXPERIMENT_PATH}/results/results.jsonl"
+    batches_file = f"{EXPERIMENT_PATH}/batches/batches.jsonl"
+    #results_file = f"{EXPERIMENT_PATH}/batch_69768be725ec81909eff329e70702946_output.jsonl"
+    #batches_file = f"{EXPERIMENT_PATH}/questionaries_arousal_valence_v01_sample_batch_0_2026-01-25-22-29.jsonl"
 
     results_content = read_jsonl(results_file)
     batches_content = read_jsonl(batches_file)
-
-    # if 'custom_id' in batches_content_file[0]:
-        # combined_data = openAI_processing(results_content_file, batches_content_file)
-    # # elif 'key' in batches_content_file[0]:
-        # # combined_data = google_processing(results_content_file, batches_content_file)
-    # # elif 'id' in batches_content_file[0]:
-        # # combined_data = huggingface_processing(results_content_file, batches_content_file)
-    # else:
-        # print("Unknown batch format, cannot process results.")
-
-    # all_fieldnames = list(combined_data[0].keys())
-
-    # df = pd.DataFrame(combined_data)
-    # df.to_excel(output_file, index=False, columns=all_fieldnames)
-
-    # print(f"Combined data written to {output_file}")
+    # Comparamos que ambos datasets tengan los mismos 'custom_id' para determinar su correspondencia
+    # (si falta algún resultado)
+    results_ids = [row['custom_id'] for row in results_content]
+    batches_ids = [row['custom_id'] for row in batches_content]
+    if collections.Counter(results_ids) == collections.Counter(batches_ids):
+        print( '"custom_id" fields in batches file and results file DO match!' )
+    else:
+        print( f'WARNING: results file (length: {len(results_ids)}) DOES NOT match batches file (length: {len(batches_ids)})' )
+        
+    excel_rows = list()
+    excel_rows_dict = dict()
+    for list_item in results_content:
+        word, sentence_id = re.match( r'questionaries_arousal_valence_task_([^_].+)_([^_].+)', list_item['custom_id']).group(1, 2)
+        column_result_name = sentence_id + '_result'
+        column_logprob_name = sentence_id + '_logprob'
+        ia_result = int(list_item['response']['body']['choices'][0]['message']['content'])
+        logprob_list = [logprob['logprob'] for logprob in list_item["response"]["body"]["choices"][0]["logprobs"]["content"][0]['top_logprobs']]
+        logprob_mean = sum(logprob_list) / len(logprob_list)
+        
+        if word in excel_rows_dict:
+           pruned_record = excel_rows[excel_rows_dict[word]]
+           pruned_record[column_result_name] = ia_result
+           pruned_record[column_logprob_name] = logprob_mean
+        else:
+           pruned_record = dict()
+           pruned_record['word'] = word
+           pruned_record[column_result_name] = ia_result
+           pruned_record[column_logprob_name] = logprob_mean
+           excel_rows_dict[word] = len(excel_rows)
+           excel_rows.append(pruned_record)
     
-    #https://medium.com/@amit25173/pandas-subset-columns-guide-071e2533918d
-    batches_table = pd.DataFrame(batches_content).loc[:, ['custom_id']]
-    print(batches_table)
-    results_table = pd.DataFrame(results_content).loc[:, ['custom_id', 'error', 'response']]
-    
-    # Hacemos merge para detectar si hemos perdido algún resultado con respecto a la entrada
-    # En ese caso habría filas que contendrían ['custom_id', NULL, NULL, ...]
-    merged_table = batches_table.merge(results_table, how = 'left', on = 'custom_id')
-    print(merged_table)
-    
-    
-    
-    #https://stackoverflow.com/questions/47561788/how-to-perform-an-operation-on-specific-columns-within-pandas-dataframe-while-pr
-    
+    pd.DataFrame(excel_rows).to_excel(output_file, index = False)
+ 
